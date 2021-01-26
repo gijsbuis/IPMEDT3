c8a546d0c31d504f9aa83994f7a017d1fb755f4d
Abel Adema <abeladema@hotmail.com>
2021-01-26 14:53:36 +0100

    lopen werkt nog steeds niet

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ window.onload = () => {
   let scene = document.getElementById('js--scene');
   
   for (let i = 0; i < places.length; i++) {
-    places[i].addEventListener('click', function(evt){
-      let att = document.createAttribute("animation");
+    places[i].addEventListener('click', function(event) {
+      let att = document.createAttribute('animation');
       att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute('position').x + " 1.6 " + this.getAttribute('position').z;
       camera.setAttribute('animation', att.value);
     });
